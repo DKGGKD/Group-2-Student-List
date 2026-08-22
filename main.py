37789544250156d58f4e2fa466ce31b99e7cc07b
cereal_buttercup <d.macaranas51160@mcc.edu.ph>
2026-08-22 05:30:54 +0000

    Updated Functions Names

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from add_features import add_student
-from delete_features import delete_student
+from add-features import add_student
+from delete-features import delete_student
 from edit import edit_student
 from search import search_student
 
